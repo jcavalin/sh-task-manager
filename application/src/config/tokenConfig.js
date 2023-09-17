e5da3f13c092aa15923d097af64de2218fa26c90
Julio Cavalin <julio.cavalin@gmail.com>
2023-09-17 17:05:18 +0100

    refactor: Improve JWT verification

--- application/src/config/tokenConfig.js
+++ application/src/config/tokenConfig.js
@@ -1,5 +1,7 @@
 import {env} from 'node:process';
 
 export default {
-    secret: env.JWT_SECRET
+    secret: env.JWT_SECRET,
+    issuer: env.JWT_ISSUER,
+    audience: env.JWT_AUDIENCE
 };
--- application/src/helpers/authorizationToken.js
+++ application/src/helpers/authorizationToken.js
@@ -2,11 +2,27 @@ import jsonwebtoken from "jsonwebtoken";
 import tokenConfig from "../config/tokenConfig.js";
 
 function generateToken(payload) {
-    return jsonwebtoken.sign(payload, tokenConfig.secret, {expiresIn: 3600});
+    return jsonwebtoken.sign(
+        payload,
+        tokenConfig.secret,
+        {
+            expiresIn: 3600,
+            audience: tokenConfig.audience,
+            issuer: tokenConfig.issuer
+        }
+    );
 }
 
 function verifyToken(token, callback) {
-    jsonwebtoken.verify(token, tokenConfig.secret, callback); // @todo Improve verification
+    jsonwebtoken.verify(
+        token,
+        tokenConfig.secret,
+        {
+            audience: tokenConfig.audience,
+            issuer: tokenConfig.issuer
+        },
+        callback
+    );
 }
 
 export {
